5623790b1d69fcd1de8765e1a94379885fc8ca97
Huang, Guangtai <guangtai@amazon.com>
2019-06-25 16:40:36 +0800

    some fix on '_np_zeros_like' add '_npi_linspace'

--- python/mxnet/_numpy_op_doc.py
+++ python/mxnet/_numpy_op_doc.py
@@ -71,7 +71,7 @@ def _np_ones_like(a):
     pass
 
 
-def _np_zeros_like(a):
+def _np_zeros_like(a, dtype=None, order='K', subok=True):
     """
     Return an array of zeros with the same shape and type as a given array.
     Parameters
@@ -101,29 +101,9 @@ def _np_zeros_like(a):
     -------
     out : ndarray
         Array of zeros with the same shape and type as `a`.
-    See Also
-    --------
-    empty_like : Return an empty array with shape and type of input.
-    ones_like : Return an array of ones with shape and type of input.
-    full_like : Return a new array with shape of input filled with value.
-    zeros : Return a new array setting values to zero.
-    Examples
-    --------
-    >>> x = np.arange(6)
-    >>> x = x.reshape((2, 3))
-    >>> x
-    array([[0, 1, 2],
-           [3, 4, 5]])
-    >>> np.zeros_like(x)
-    array([[0, 0, 0],
-           [0, 0, 0]])
-    >>> y = np.arange(3, dtype=float)
-    >>> y
-    array([0., 1., 2.])
-    >>> np.zeros_like(y)
-    array([0.,  0.,  0.])
-
-    Note: parameter 'dtype', 'order', 'subok', and 'shape' not support
+    Notes
+    -----
+    parameter 'dtype', 'order', 'subok', and 'shape' not support
     """
     pass
 
@@ -177,6 +157,54 @@ def _npi_multinomial(a):
     out : ndarray
         The drawn samples, of shape size, if that was provided. If not, the shape is ``(N,)``.
         In other words, each entry ``out[i,j,...,:]`` is an N-dimensional value drawn from the distribution.
+    """
+    pass
+
+
+def _npi_linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0, **kwargs):  # pylint: disable=too-many-arguments
+    """Return evenly spaced numbers over a specified interval.
+
+    Returns num evenly spaced samples, calculated over the interval [start, stop].
+    The endpoint of the interval can optionally be excluded.
+
+    Parameters
+    ----------
+    start : array_like
+        The starting value of the sequence.
+    stop : array_like
+        The end value of the sequence, unless endpoint is set to False. In
+        that case, the sequence consists of all but the last of num + 1
+        evenly spaced samples, so that stop is excluded. Note that the step
+        size changes when endpoint is False.
+    num : int, optional
+        Number of samples to generate. Default is 50. Must be non-negative.
+    endpoint : bool, optional
+        If True, stop is the last sample. Otherwise, it is not included.
+        Default is True.
+    retstep : bool, optional
+        If True, return (samples, step), where step is the spacing between samples.
+    dtype : dtype, optional
+        The type of the output array. If dtype is not given, infer the data
+        type from the other input arguments.
+    axis : int, optional
+        The axis in the result to store the samples. Relevant only if start or
+        stop are array-like. By default (0), the samples will be along a new
+        axis inserted at the beginning. Use -1 to get an axis at the end.
+
+    Returns
+    -------
+    samples : ndarray
+        There are num equally spaced samples in the closed interval
+        `[start, stop]` or the half-open interval `[start, stop)`
+        (depending on whether endpoint is True or False).
+    step : float, optional
+        Only returned if retstep is True
+        Size of spacing between samples.
+
+    Notes
+    -----
+    'start' and 'stop' do not support list and numpy array
+    axis could only be 0
     """
     pass
 
